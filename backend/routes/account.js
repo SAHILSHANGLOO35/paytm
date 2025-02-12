c714444840eb8b9d1a7eae6e5a73704b3b7b5e8f
Sahil Shangloo <sahilshangloo35@gmail.com>
2025-02-12 23:31:14 +0530

    Transactions endpoint done

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/index.js";
 import { Account } from "../db.js";
+import mongoose from "mongoose";
 
 export const accountRouter = Router();
 
 accountRouter.get("/balance", authMiddleware, async (req, res) => {
     try {
-        const account = await Account.findOne({userId: req.userId});
+        const account = await Account.findOne({ userId: req.userId });
         const balance = account.balance;
 
         res.status(200).json({
@@ -19,6 +20,50 @@ accountRouter.get("/balance", authMiddleware, async (req, res) => {
     }
 });
 
-accountRouter.post("/transfer", (req, res) => {
-    
-})
+accountRouter.post("/transfer", authMiddleware, async (req, res) => {
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+    const { amount, to } = req.body;
+
+    try {
+        const account = await Account.findOne({ userId: req.userId }).session(
+            session
+        );
+
+        if (!account || account.balance < amount) {
+            await session.abortTransaction();
+            return res.status(400).json({
+                message: "Insuffecient balance",
+            });
+        }
+        const toAccount = await Account.findOne({ userId: to }).session(
+            session
+        );
+
+        if (!toAccount) {
+            await session.abortTransaction();
+            return res.status(400).json({
+                message: "Invalid account",
+            });
+        }
+        await Account.updateOne(
+            { userId: req.userId },
+            { $inc: { balance: -amount } }
+        ).session(session);
+        await Account.updateOne(
+            { userId: to },
+            { $inc: { balance: amount } }
+        ).session(session);
+
+        await session.commitTransaction();
+
+        res.status(200).json({
+            message: "Transfer successful",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Some internal error",
+        });
+    }
+});
